4d650d6412282f43adf04d63f98e5a052c7fedc7
collins ike <oldboyprint@gmail.com>
2022-09-08 22:02:25 -0700

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Print a strint using followed by anew line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
