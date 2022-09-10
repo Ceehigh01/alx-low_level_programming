4faa08feff4ba70af3b01216959d195723b41d4b
Collins Ike <cizones1@gmail.com>
2022-09-10 14:15:00 +0100

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,39 +1,11 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Description: Print a string using followed by a new line
- * Return: Always 0 (Success)
+ *main - Prints quote
+ *Return: zero on success
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-=======
-#include <studio.h>
-
-
-
-/**
- *
- * *main - Entry point
- *
- * *
- *
- * *Return: Always 0
- *
- * */
-
-int main(void)
-
-{
-
-	puts("\"Programming is like building a multilingual puzzle");
-
-
-
-	return (0);
-
->>>>>>> 2ad93cdecc9d2e0318b181cd44f3af314f4d8624
 }
